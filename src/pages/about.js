2603b6cf877d54e1023dbc36c24dbbb3e987ad1d
David Rider <riderda@oregonstate.edu>
2020-03-08 02:29:54 +0900

    Add files via upload

---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import '../App.css'
+
+class About extends Component {
+
+	render() {
+		return (
+    		<div className="About">
+        		<header className="About-header">
+     				<h2>What is Synapse?</h2>
+     			</header>
+      		
+	      		<body>
+	      			<div className="Description">
+	      				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      			</div>
+	      		</body>
+	      	</div>
+		);
+	}
+
+}
+
+export default About
