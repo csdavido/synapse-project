5ddc52e30f43795d95bc3c8d5d532c2b2e3b58a8
David Rider <riderda@oregonstate.edu>
2020-03-12 19:26:12 +0900

    Update feed.js
    -fixed user's thought feed

--- src/feed.js
+++ src/feed.js
@@ -50,6 +50,7 @@ class Thoughts extends Component {
 
     //const synapse = new web3.eth.Contract(SYNAPSE_ABI, SYNAPSE_ADDRESS)
     const profile = new web3.eth.Contract(PROFILE_ABI, PROFILE_ADDRESS)
+    const accounts = await web3.eth.getAccounts()
 
     this.setState({ profile })
     this.setState({ thoughtCount })
@@ -58,13 +59,13 @@ class Thoughts extends Component {
 
     this.setState({ loading: true })
 
-    const add = this.state.address
+    const add = accounts[0]
 
     for (var i = thoughtCount - 1; i >= 0; i--) {
       
       const singleThought = await profile.methods.thoughts(i).call()
 
-      if (singleThought.address === add) {
+      if (singleThought.sender == add) {
         this.setState({
           thoughts: [...this.state.thoughts, singleThought]
         })
@@ -122,9 +123,9 @@ class Thoughts extends Component {
 
                     <div class="thought-counts">
 
-                      <div class="comments">
-                        <svg class="feather feather-message-circle sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
-                        <div class="comment-num">0</div>
+                      <div class="likes">
+                        <svg class="feather feather-heart sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                        <div class="like-num">0</div>
                       </div>
 
                       <div class="shares">
@@ -136,7 +137,6 @@ class Thoughts extends Component {
                         <svg class="feather feather-heart sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
                         <div class="like-num">0</div>
                       </div>
-
                     </div>
                   </div>
                 </div>
