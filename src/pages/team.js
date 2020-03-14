ad8b2859db635aec40bbd2e56a610504edeaec83
David Rider <riderda@oregonstate.edu>
2020-03-14 09:59:26 +0900

    Update team.js
    -added bio(csdavido)

--- src/pages/team.js
+++ src/pages/team.js
@@ -21,7 +21,7 @@ class Team extends Component {
 	      				<h3>Andrew Ngo</h3>
 	      				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 	      				<h3>David Rider</h3>
-	      				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      				<p>David is a software engineer, security nerd, and cryptocurrency enthusiast. He lives in South Korea and plays way too many video games. You can email him at riderda@oregonstate.edu</p>
 	      				<h3>Reuben Youngblom</h3>
 	      				<p>Reuben is a CS major and lives in CA. He likes blockchain and dogs and is a terrible cook. He can be reached at youngblr@oregonstate.edu</p>
 
